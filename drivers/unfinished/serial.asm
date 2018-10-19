9045def02fa0462ebda589b458a77c6c5e636b23
Alexey Ryabov <6006l1k@gmail.com>
2018-10-19 18:44:26 +0300

    Improve driver

--- drivers/unfinished/serial.asm
+++ drivers/unfinished/serial.asm
@@ -15,6 +15,82 @@ __DEBUG__ = 1
 __DEBUG_LEVEL__ = L_DBG
 API_VERSION = 1
 
+; Registers offset
+UART_REG_RBR_RO = 0x00 ; Receive Buffer Register
+UART_REG_THR_WO = 0x00 ; Transmitter Holding Register
+UART_REG_DL_LSB = 0x00 ; Divisor Latch (LSB)
+UART_REG_IER = 0x01 ; Interrupt Enable Register
+UART_REG_DL_MSB = 0x01 ; Divisor Latch (MSB)
+UART_REG_IIR_RO = 0x02 ; Interrupt Indentification Register
+UART_REG_FCR_WO = 0x02 ; FIFO Control Register
+UART_REG_LCR = 0x03 ; Line Control Register
+UART_REG_MCR = 0x04 ; Modem Control Register
+UART_REG_LSR = 0x05 ; Line Status Register
+UART_REG_MSR = 0x06 ; Modem Status Register
+UART_REG_SCR = 0x07 ; Scratch Register
+
+UART_FLAG_IER_DR = 0x00 ; Data Ready Interrupt
+UART_FLAG_IER_THRE = 0x01 ; THR Empty Interrupt
+UART_FLAG_IER_LS = 0x02 ; Line Status Interrupt
+UART_FLAG_IER_DSS = 0x03 ; Delta Status Signals Interrupt
+
+UART_FLAG_IIR_NO_INT = 0x00
+UART_FLAG_IIR_IID0 = 0x01
+UART_FLAG_IIR_IID1 = 0x02
+UART_FLAG_IIR_IID2 = 0x03
+UART_FLAG_IIR_IID_MASK = 0x0E
+
+UART_FLAG_FCR_EN = 0x00 ; FIFO enable
+UART_FLAG_FCR_CLR_RX = 0x01 ; Clear recevier FIFO
+UART_FLAG_FCR_CLR_TX = 0x02 ; Clear transmitter FIFO
+UART_FLAG_FCR_DMA = 0x03 ; DMA mode
+UART_FLAG_FCR_TRIG_LVL0 = 0x04 ; Trigger level of the DR-interrupt
+UART_FLAG_FCR_TRIG_LVL1 = 0x05
+
+UART_FLAG_LCR_WL0 = 0x00 ; Word length
+UART_FLAG_LCR_WL1 = 0x01
+UART_FLAG_LCR_SB = 0x02 ; Stop bits
+UART_FLAG_LCR_PE = 0x03 ; Parity enable
+UART_FLAG_LCR_ES = 0x04 ; Even Parity select
+UART_FLAG_LCR_SP = 0x05 ; Stick Parity select
+UART_FLAG_LCR_SBR = 0x06
+UART_FLAG_LCR_DLAB = 0x07
+
+UART_FLAG_MCR_DTR = 0x00
+UART_FLAG_MCR_RTS = 0x01
+UART_FLAG_MCR_OUT1 = 0x02
+UART_FLAG_MCR_OUT2 = 0x03
+UART_FLAG_MCR_LOOP = 0x04
+
+UART_FLAG_LSR_RBF = 0x00
+UART_FLAG_LSR_OE = 0x01
+UART_FLAG_LSR_PE = 0x02
+UART_FLAG_LSR_FE = 0x03
+UART_FLAG_LSR_BREAK = 0x04
+UART_FLAG_LSR_THRE = 0x05
+UART_FLAG_LSR_TEMT = 0x06
+UART_FLAG_LSR_FIFO_ERR = 0x07
+
+UART_FLAG_MSR_DCTS = 0x00
+UART_FLAG_MSR_DDSR = 0x01
+UART_FLAG_MSR_TERI = 0x02
+UART_FLAG_MSR_DDCD = 0x03
+UART_FLAG_MSR_CTS = 0x04
+UART_FLAG_MSR_DSR = 0x05
+UART_FLAG_MSR_RI = 0x06
+UART_FLAG_MSR_DCD = 0x07
+
+UART_TEST_SIGNALS = (1 shl UART_FLAG_MCR_DTR) + \
+                    (1 shl UART_FLAG_MCR_RTS) + \
+                    (1 shl UART_FLAG_MCR_OUT1) + \
+                    (1 shl UART_FLAG_MCR_OUT2) + \
+                    (1 shl UART_FLAG_MCR_LOOP)
+
+UART_CHECK_MASK = (1 shl UART_FLAG_MSR_CTS) + \
+                  (1 shl UART_FLAG_MSR_DSR) + \
+                  (1 shl UART_FLAG_MSR_RI) + \
+                  (1 shl UART_FLAG_MSR_DCD)
+
 section '.flat' readable writable executable
 
 include '../struct.inc'
@@ -29,18 +105,38 @@ struct port serial_port
         io_addr         dd ? ; base address of io port
 ends
 
-drv_funcs:
-        dd drv_startup
-        dd drv_shutdown
+; dx = base io
+; al = result
+macro rd_reg reg
+{
+        push    edx
+        add     dx, reg
+        in      al, dx
+        pop     edx
+}
+
+; dx = base io
+; al = new value
+macro wr_reg reg
+{
+        push    edx
+        add     dx, reg
+        out     dx, al
+        pop     edx
+}
 
 proc START c, state:dword, cmdline:dword
         cmp     [state], 1
-        jne     .exit
-        call    detect
-        invoke  RegService, drv_name, service_proc
-        ret
-.exit:
+        je      @f
         xor     eax, eax
+        ret
+
+@@:
+        stdcall add_port, 0x3f8, 4
+        stdcall add_port, 0x2f8, 3
+        stdcall add_port, 0x3e8, 4
+        stdcall add_port, 0x2e8, 3
+        invoke  RegService, drv_name, service_proc
         ret
 endp
 
@@ -66,9 +162,7 @@ proc service_proc stdcall, ioctl:dword
         ret
 endp
 
-; return not null, if success
-proc add_port uses ebx edi, io_addr:dword
-        DEBUGF  L_DBG, "Serial: found serial port 0x%x\n", [io_addr]
+proc add_port stdcall uses ebx edi, io_addr:dword, irqn:dword
         xor     ebx, ebx ; 0 = reserve
         mov     ecx, [io_addr]
         lea     edx, [ecx + 7]
@@ -80,34 +174,71 @@ proc add_port uses ebx edi, io_addr:dword
         DEBUGF  L_ERR, "Serial: failed to reserve ports\n"
         jmp     .err
 @@:
+        mov     edx, [io_addr]
+
+        ; enable loopback
+        mov     al, (1 shl UART_FLAG_MCR_LOOP)
+        wr_reg  UART_REG_MCR
+
+        ; read status
+        rd_reg  UART_REG_MSR
+        and     al, UART_CHECK_MASK
+        test    al, al
+        jnz     .free_port
+
+        ; set test signals
+        mov     al, UART_TEST_SIGNALS
+        wr_reg  UART_REG_MCR
+
+        ; check signals
+        rd_reg  UART_REG_MSR
+        and     al, UART_CHECK_MASK
+        cmp     al, UART_CHECK_MASK
+        jnz     .free_port
+
+        DEBUGF  L_DBG, "Serial: found serial port 0x%x\n", [io_addr]
+
+        ; initialize port
+        xor     eax, eax
+        wr_reg  UART_REG_MCR
+        wr_reg  UART_REG_IER
+
+        ; create descriptor
         invoke  KernelAlloc, sizeof.port
         test    eax, eax
-        jz      .nomem
+        jz      .free_port
         mov     edi, eax
         push    edi
+
         ; clear allocated memory
         xor     eax, eax
         mov     ecx, sizeof.port
         cld
         rep stosb
         pop     edi
+
         ; fill
         mov     eax, drv_funcs
         mov     [edi + port.funcs], eax
         mov     eax, [io_addr]
         mov     [edi + port.io_addr], eax
+
+        invoke  AttachIntHandler, [irqn], irq_handler, edi
+        test    eax, eax
+        jz      .free_mem
+
         ; add device
         invoke  SerialAddPort, edi
         test    eax, eax
-        jnz     .fail
+        jnz     .free_mem
         mov     eax, edi
         ret
 
-.fail:
-        DEBUGF  L_ERR, "Serial: add device failed with code=%d\n", eax
+.free_mem:
+        DEBUGF  L_DBG, "Serial: add port 0x%x failed\n", [io_addr]
         invoke  KernelFree, edi
 
-.nomem:
+.free_port:
         xor     ebx, ebx
         inc     ebx ; 1 = release
         mov     ecx, [io_addr]
@@ -121,71 +252,56 @@ proc add_port uses ebx edi, io_addr:dword
         ret
 endp
 
-; edx = addr
-uart_test:
-        ; read MCR and save old value in ah
-        add     edx, 4
-        in      al, dx
-        shl     ax, 8
-        ; enable loopback
-        mov     al, 0x10
-        out     dx, al
-        ; read MSR
-        add     dx, 2
-        in      al, dx
-        and     al, 0xf0
-        test    al, al
-        jnz     .unavailable
-        ; set RTS, DTR
-        sub     dx, 2
-        mov     al, 0x1f
-        out     dx, al
-        ; check for RTS, DTR
-        add     dx, 2
-        in      al, dx
-        and     al, 0xf0
-        cmp     al, 0xf0
-        jz      .found
+proc irq_handler c, user_data:dword
+locals
+        res     dd ? ; return 0 if no uart int pending
+endl
+        and     [res], 0
+        mov     ecx, [user_data]
+        mov     edx, [ecx + port.io_addr]
+.read_iir:
+        rd_reg  UART_REG_IIR_RO
+        test    al, (1 shl UART_FLAG_IIR_NO_INT)
+        jnz     .exit
+        inc     [res]
+        and     ax, UART_FLAG_IIR_IID_MASK
+        shr     ax, 1
+        ; check source
+        test    ax, ax
+        jz      .modem
+        cmp     ax, 1
+        jz      .xmit
+        cmp     ax, 2
+        jz      .recv
+        cmp     ax, 3
+        jz      .status
+        jmp     .read_iir
 
-.unavailable:
-        xor     eax, eax
-        ret
-.found:
-        ; restore MCR
-        shr     ax, 8
-        out     dx, al
-        xor     eax, eax
-        inc     eax
-.return:
-        ret
+.modem:
+        ; read MSR for clear interrupt
+        rd_reg  UART_REG_MSR
+        jmp     .read_iir
 
-detect:
-        mov     edx, 0x3F8
-        call    uart_test
-        test    eax, eax
-        jz      @f
-        stdcall add_port, 0x3F8
-@@:
-        mov     edx, 0x2F8
-        call    uart_test
-        test    eax, eax
-        jz      @f
-        stdcall add_port, 0x2F8
-@@:
-        mov     edx, 0x3E8
-        call    uart_test
-        test    eax, eax
-        jz      @f
-        stdcall add_port, 0x3E8
-@@:
-        mov     edx, 0x2E8
-        call    uart_test
-        test    eax, eax
-        jz      @f
-        stdcall add_port, 0x2E8
-@@:
-        DEBUGF  L_DBG, "Serial: ports scan completed\n"
+.xmit:
+        ; write byte or disable THRE int
+        jmp     .read_iir
+
+.recv:
+        ; read byte
+        rd_reg  UART_REG_RBR_RO
+        jmp     .read_iir
+
+.status:
+        rd_reg  UART_REG_LSR
+        jmp     .read_iir
+
+.fifo:
+        jmp     .read_iir
+
+.exit:
+        mov     eax, [res]
         ret
+endp
 
 proc drv_startup stdcall, desc:DWORD
         DEBUGF  L_DBG, "Serial: open 0x%x\n", [desc]
@@ -199,6 +315,13 @@ endp
 
 version     dd  0x0000001
 drv_name    db 'SERIAL', 0
+
+align 4
+drv_funcs:
+        dd drv_funcs_end - drv_funcs
+        dd drv_startup
+        dd drv_shutdown
+drv_funcs_end:
 
 include_debug_strings
 
